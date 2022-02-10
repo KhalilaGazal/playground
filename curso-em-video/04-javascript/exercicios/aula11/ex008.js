40e4e2f8e1166d410aa451f232d8d9c8c25013ca
khalilagazal <khalilagazal@outlook.com>
2022-02-09 21:10:20 -0300

    Add módulo D

---
+++ curso-em-video/04-javascript/exercicios/aula11/ex008.js
@@ -0,0 +1,10 @@
+var velocidade = 60.5
+
+console.log(`A velocidade do seu carro é ${velocidade}km/h`)
+
+// Condição simples
+if (velocidade > 60) { 
+  console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre utilizando cinto de segurança!`)
---
+++ curso-em-video/04-javascript/exercicios/aula11/ex009.js
@@ -0,0 +1,11 @@
+var país = 'Brasil'
+
+console.log(`Vivendo em ${país}`)
+
+// Condição composta
+if (país == 'Brasil') {
+  console.log('Você é brasileiro!')
+}
+else {
+  console.log('Você é estrangeiro!')
+}
