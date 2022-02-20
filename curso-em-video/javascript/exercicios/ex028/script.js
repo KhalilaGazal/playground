b1620a1a8d9e455acab827054601379388a4df12
khalilagazal <khalilagazal@outlook.com>
2022-02-20 20:59:58 -0300

    Fix ex028

--- curso-em-video/javascript/exercicios/ex028/script.js
+++ curso-em-video/javascript/exercicios/ex028/script.js
@@ -3,7 +3,7 @@ function calcular() {
   let numero = Number(window.document.getElementById('numero').value)
   let fatorial = 1
 
-  resultado.innerHTML += `<h2>Calculando <span>${numero}!</span></h2>`
+  resultado.innerHTML = `<h2>Calculando <span>${numero}!</span></h2>`
 
   for (let i = numero; i >= 1; i--) {
     fatorial *= i
